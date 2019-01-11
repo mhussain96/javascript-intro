30fdb658bcac1c1664743d31e43c8021507aaf68
Maiwand <mhussain2@spartaglobal.com>
2019-01-11 11:49:32 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// alert ("Hello me!");
+console.log("Hi in console");
+var myNumber = 90;
+console.log(myNumber);
+
+var myValue = 50;
+
+console.log(myNumber*myValue);
+
+myNumber = "Hello ";
+var mySecond = "World";
+
+console.log(myNumber + mySecond + " plus ");
+
+console.log("Number ", myNumber, " plus ", myNumber, );
+
+var myValue = [50, 7, true]
